882249a3bc62dd4ca3fd8e570b390ed04da3a30b
Ihor Melnyk <73445190+Mentos302@users.noreply.github.com>
2021-10-22 15:41:36 +0300

    pravki 22.10

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -9,10 +9,10 @@ fulls.forEach((e) => {
     </p>`
 })
 
-const readmoreBtns = document.querySelectorAll('#readmore')
+const readmoreBtns = document.querySelectorAll('.feedback__item')
 readmoreBtns.forEach((e) => {
-  e.addEventListener('click', (ev) => {
-    const parent = ev.target.parentElement
+  const showFeedback = (ev) => {
+    const parent = ev.target.querySelector('.feedback__item-wrapper')
 
     const paragraphs = parent.querySelector('.feedback__body').children
 
@@ -20,8 +20,38 @@ readmoreBtns.forEach((e) => {
     parent.style.zIndex = '4'
     paragraphs[0].style.display = 'block'
     paragraphs[1].style.display = 'none'
-    ev.target.style.display = 'none'
-  })
+    ev.target.querySelector('#readmore').style.display = 'none'
+  }
+
+  const hideFeedback = (ev) => {
+    const parent = ev.target.querySelector('.feedback__item-wrapper')
+
+    const paragraphs = parent.querySelector('.feedback__body').children
+
+    parent.style.position = 'relative'
+    parent.style.zIndex = '1'
+    paragraphs[0].style.display = 'none'
+    paragraphs[1].style.display = 'block'
+    ev.target.querySelector('#readmore').style.display = 'block'
+  }
+
+  if (window.innerWidth > 500) {
+    e.addEventListener('mouseenter', showFeedback)
+    e.addEventListener('mouseleave', hideFeedback)
+  } else {
+    let isOpen
+    const toggleOpen = (ev) => {
+      if (isOpen) {
+        isOpen = false
+        hideFeedback(ev)
+      } else {
+        isOpen = true
+        showFeedback(ev)
+      }
+    }
+
+    e.addEventListener('click', toggleOpen)
+  }
 })
 
 const ecosystemBtns = document.querySelectorAll('.ecosystem-switcher-item')
@@ -41,8 +71,9 @@ ecosystemBtns.forEach((e) => {
   })
 })
 
-if (window.innerWidth < 800) {
+if (window.innerWidth < 838) {
   const el = document.querySelector('.facts__inner')
+
   const el1 = el.children[0].children[1]
   const el2 = el.children[1].children[0]
 
@@ -60,7 +91,7 @@ if (window.innerWidth < 800) {
   children[1].outerHTML = ''
 }
 
-if (window.innerWidth < 800 && window.innerWidth > 500) {
+if (window.innerWidth < 800 || window.innerWidth > 500) {
   const cards = document.querySelectorAll('.asbuyer__card')
 
   cards.forEach((e) => {
@@ -73,59 +104,162 @@ if (window.innerWidth < 800 && window.innerWidth > 500) {
   })
 }
 
-if (window.innerWidth < 1000) {
-  console.log(`PENIS`)
-  ;(function generateMonetSwiper() {
-    const wrapper = document.querySelector('.monetization__inner')
+if (window.innerWidth < 500) {
+  const el = document.querySelector('.mediaSwiper .mediaSlider-btns')
 
-    wrapper.innerHTML = `
-        <div class="swiper monetizationSwiper">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <img src="assets/img/monetization/Shopping-cart.svg" alt="" />
-            <b>Маркетплейс товаров</b>
-            <p>
-                Мы получаем деньги от покупателей, а также комиссии от
-                торговцев
-            </p>
-          </div>
-          <div class="swiper-slide"><img src="assets/img/monetization/Wallet.svg" alt="" />
-          <b>Финансовые инструменты</b>
-          <p>
-            Где все стороны (инвесторы, стартапы, наши Платформы) могут
-            хорошо зарабатывать
-          </p></div>
-          <div class="swiper-slide"><img src="assets/img/monetization/Activity.svg" alt="" />
-          <b>Маркетплейс игр с DeFi </b>
-          <p>
-            Позволяющие инвесторам, стартапам, и нашим платформам хорошо
-            зарабатывать
-          </p></div>
-          <div class="swiper-slide"><img src="assets/img/monetization/Change.svg" alt="" />
-          <b>Обмен криптовалют </b>
-          <p>
-            Наши пользователи обменивают криптовалюту на ФИАТ, а ФИАТ на
-            криптовалюту. Мы получаем комиссию за обмен в обе стороны.
-          </p>
-          </div>
-          <div class="swiper-slide"><img src="assets/img/monetization/Gamee.svg" alt="" />
-            <b>Естественный рост (ZLW) токена</b>
-            <p>
-                Мы не проводили ICO, IEO, pre-sale, airdrops, не давали скидок и
-                т. д. Все разработки выполнялись за собственные средства.
-                Поэтому ничто не влияет на нашу цену сейчас! И мы растем без
-                всякого давления
-            </p>
-          </div>
-        </div>
-        <div class="swiper-pagination"></div>
-      </div>
-        `
+  const htmlBuffer = `${el.outerHTML}`
+  el.outerHTML = ``
 
-    var swiper = new Swiper('.mySwiper', {
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    })
-  })()
+  document.querySelector('.mediaSwiper').innerHTML += htmlBuffer
 }
+
+if (window.innerWidth < 500) {
+  console.log(`PENIS`)
+  const switchersWrapper = document.querySelector('.ecosystem-switchers')
+  const switchers = document.querySelectorAll('.ecosystem-switchers > *')
+  let slides = ``
+
+  switchers.forEach((e) => {
+    slides += `
+      <div class="swiper-slide">
+          ${e.outerHTML}
+      </div>
+    `
+  })
+
+  switchersWrapper.innerHTML = `
+    <div class="swiper ecosystemSwiper">
+      <div class="swiper-wrapper">
+        ${slides}
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+  `
+}
+
+if (window.innerWidth < 500) {
+  let buffer = ''
+  const wrapper = document.querySelectorAll('.footer__head > *')
+
+  buffer = `${wrapper[0].outerHTML}`
+
+  wrapper[0].outerHTML = wrapper[1].outerHTML
+  wrapper[1].outerHTML = buffer
+}
+
+new Swiper('.mediaSwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 20,
+    // },
+    // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 3,
+    //   spaceBetween: 30,
+    // },
+    // when window width is >= 640px
+    800: {
+      slidesPerView: 3,
+    },
+  },
+})
+
+new Swiper('.partnersSwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  slidesPerView: 1.3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 640px
+    500: {
+      slidesPerView: 2.7,
+      spaceBetween: 25,
+    },
+    800: {
+      slidesPerView: 4.5,
+      spaceBetween: 25,
+    },
+    1200: {
+      slidesPerView: 6.2,
+      spaceBetween: 25,
+    },
+  },
+})
+new Swiper('.monetizationSwiper', {
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  spaceBetween: 16,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
+
+new Swiper('.earningSwiper', {
+  spaceBetween: 23,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
+
+const ecosystemSlider = new Swiper('.ecosystemSwiper', {
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  spaceBetween: 23,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
+
+ecosystemSlider.on('slideChange', function (ev) {
+  const switchers = document.querySelectorAll(
+    '.ecosystemSwiper .ecosystem-switcher-item'
+  )
+  console.log(ev.realIndex)
+
+  const changeActive = (slug) => {
+    document
+      .querySelectorAll(`.ecosystem-switcher-item.active`)
+      .forEach((e) => {
+        e.classList.remove('active')
+      })
+    document
+      .querySelector('.ecosystem__layout.active')
+      .classList.remove('active')
+
+    document.getElementById(`${slug}__content`).classList.add('active')
+    document.querySelectorAll(`#${slug}`).forEach((e) => {
+      e.classList.add('active')
+    })
+  }
+
+  switch (ev.realIndex) {
+    case 0:
+      changeActive('marketplace')
+      break
+    case 1:
+      changeActive('games')
+      break
+    case 2:
+      changeActive('exchange')
+      break
+    case 3:
+      changeActive('fintech')
+      break
+  }
+})
